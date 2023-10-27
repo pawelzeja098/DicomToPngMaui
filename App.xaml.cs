c4de6e0abca75c7c445fd57b778e702c320bac03
Pawel <pawel.zeja@infokam.kielce.pl>
2023-10-27 18:46:19 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DicomToPngMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DicomToPngMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
