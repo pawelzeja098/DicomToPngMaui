38645e9a57c50e0846248543d06ddea98fc9fadf
Pawel <pawel.zeja@infokam.kielce.pl>
2023-10-27 23:11:26 +0200

    Added new page filepicker ERROR: MainPage.xaml

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -1,9 +1,72 @@
+
+using Microsoft.Maui.Controls;
+using System.Diagnostics;
 namespace DicomToPngMaui.Pages;
+
+
+using Xamarin.Essentials;
+    
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        async Task<FileResult> PickAndShow(PickOptions options)
+        {
+            try
+            {
+                var result = await FilePicker.PickAsync(options);
+                if (result != null)
+                {
+                    //Text = $"File Name: {result.FileName}";
+                    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var stream = await result.OpenReadAsync();
+                        var Image = ImageSource.FromStream(() => stream);
+
+
+                        byte[] fileBytes = new byte[stream.Length];
+                        await stream.ReadAsync(fileBytes, 0, (int)stream.Length);
+                        string fileName = result.FileName;
+                        string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), fileName);
+                        File.WriteAllBytes(filePath, fileBytes);
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // The user canceled or something went wrong
+            }
+
+            return null;
+        }
+
+        var customFileType =
+    new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+    {
+        { DevicePlatform.iOS, new[] { "public.my.comic.extension" } }, // or general UTType values
+        { DevicePlatform.Android, new[] { "application/comics" } },
+        { DevicePlatform.UWP, new[] { ".cbr", ".cbz" } },
+        { DevicePlatform.Tizen, new[] { "*/*" } },
+        { DevicePlatform.macOS, new[] { "cbr", "cbz" } }, // or general UTType values
+    });
+        var options = new PickOptions
+        {
+            PickerTitle = "Please select a comic file",
+            FileTypes = customFileType,
+        };
+        await PickAndShow(options);
+    }
+    
+
+
+
+
 }
