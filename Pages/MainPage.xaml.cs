206e8f0e0211e0f48bf30b50e59dce4d84ada9d3
Pawel <pawel.zeja@infokam.kielce.pl>
2023-12-01 15:08:28 +0100

    Download files from url, added progress bar

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Net;
 using FellowOakDicom;
 using FellowOakDicom.Imaging;
 using SkiaSharp;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 
 namespace DicomToPngMaui.Pages;
@@ -15,10 +16,13 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+
+        LoadImages();
     }
 
     [Obsolete]
     private async void OnFilePickerClicked(object sender, EventArgs e)
+    
     {
         var customFileType =
     new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
@@ -30,68 +34,74 @@ public partial class MainPage : ContentPage
             { DevicePlatform.macOS, new[] { "png", "jpg" ,"dcm","DCM"} }, // or general UTType values
     });
 
-        if (Device.RuntimePlatform == Device.Android)
+        downloadProgressBar.IsVisible = true;
+        var url = YourUrlEntry.Text;
+
+        if (url != null)
         {
             try
             {
-                var url = YourUrlEntry.Text;
+                downloadProgressBar.Progress = 0.1;
 
                 using (var httpClient = new HttpClient())
                 {
                     // Get data from URL
                     var data = await httpClient.GetByteArrayAsync(url);
 
+                    downloadProgressBar.Progress = 0.5;
 
                     var fileName = "DownloadedFile";
                     var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileName);
                     File.WriteAllBytes(filePath, data);
 
-                    await DisplayAlert("Sukces", $"File downloaded and saved in: {fileName}", "OK");
+                    await DisplayAlert("Success", $"File downloaded and saved in: {fileName}", "OK");
 
                     UnpackDicom(filePath);
                 }
             }
             catch (Exception ex)
             {
-                await DisplayAlert("B³¹d", $"Wyst¹pi³ b³¹d: {ex.Message}", "OK");
+                await DisplayAlert("Error", $"Error ocurred: {ex.Message}", "OK");
             }
         }
 
-        var result = await FilePicker.PickAsync(new PickOptions
+        else
         {
-            PickerTitle = "Please select a file",
-            FileTypes = customFileType,
-        });
-        if (result == null)
-            return; // user canceled file picking
-  
-           
-        var stream = await result.OpenReadAsync();
+            var result = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Please select a file",
+                FileTypes = customFileType,
+            });
+            if (result == null)
+                return; // user canceled file picking
 
-        //myImage.Source = ImageSource.FromStream(() => stream);
 
-        string tmpFolderPath = GetTmpFolderPath();
+            var stream = await result.OpenReadAsync();
 
-        // Create a target file path in the tmp folder
-        string destinationFilePath = Path.Combine(tmpFolderPath, result.FileName);
+            //myImage.Source = ImageSource.FromStream(() => stream);
 
-        // Save the stream to the file
-        using (FileStream fileStream = File.Create(destinationFilePath))
-        {
-            await stream.CopyToAsync(fileStream);
-            fileStream.Close();
+            string tmpFolderPath = GetTmpFolderPath();
+
+            // Create a target file path in the tmp folder
+            string destinationFilePath = Path.Combine(tmpFolderPath, result.FileName);
+
+            // Save the stream to the file
+            using (FileStream fileStream = File.Create(destinationFilePath))
+            {
+                await stream.CopyToAsync(fileStream);
+                fileStream.Close();
+            }
+
+            MemoryStream copyStream = new MemoryStream();
+            stream.Seek(0, SeekOrigin.Begin);
+            await stream.CopyToAsync(copyStream);
+
+
+
+
+
+            UnpackDicom(destinationFilePath);
         }
-        
-        MemoryStream copyStream = new MemoryStream();
-        stream.Seek(0, SeekOrigin.Begin);
-        await stream.CopyToAsync(copyStream);
-
-        
-
-        
-        
-        UnpackDicom(destinationFilePath);
-       
     }
 
     private async void UnpackDicom(string fileStream)
@@ -110,7 +120,7 @@ public partial class MainPage : ContentPage
             
             int numberOfFrames = pixelData.NumberOfFrames;
 
-            
+
             var path = FileSystem.Current.AppDataDirectory;
 
             for (int frameIndex = 0; frameIndex < numberOfFrames; frameIndex++)
@@ -125,7 +135,7 @@ public partial class MainPage : ContentPage
                 {
                     // Save file into PNG format.
                     string outputImagePath = Path.Combine(path, $"frame_{frameIndex + 1}.png"); // frame_1.jpg, frame_2.jpg, ...
-
+                    downloadProgressBar.Progress = 0.9;
                     using (var imageStream = File.OpenWrite(outputImagePath))
                     {
                         skBitmap.Encode(imageStream, SKEncodedImageFormat.Png, 100);
@@ -134,14 +144,41 @@ public partial class MainPage : ContentPage
 
             }
 
+            
 
             await DisplayAlert("Alert", "Finished unpacking dicom", "OK");
             Console.WriteLine("Finished unpacking dicom");
+            downloadProgressBar.Progress = 1;
+            LoadImages();
         }
         else
         {
             await DisplayAlert("Alert", "There is no image data in the DICOM file.", "OK");
             Console.WriteLine("There is no image data in the DICOM file.");
+        }
+    }
+
+    private void LoadImages()
+    {
+        var path = FileSystem.Current.AppDataDirectory;
+
+        imageStackLayout.Children.Clear();
+
+        // Get a list of PNG files in the specified directory
+        var pngFiles = Directory.GetFiles(path, "frame_*.png");
+
+        foreach (var pngFile in pngFiles)
+        {
+            // Create Image and set its source to the loaded PNG file
+            var image = new Image
+            {
+                Source = ImageSource.FromFile(pngFile),
+                WidthRequest = 300, // Adjust width if needed
+                HeightRequest = 300 // Adjust height if needed
+            };
+
+            // Add the Image to the StackLayout
+            imageStackLayout.Children.Add(image);
         }
     }
 
